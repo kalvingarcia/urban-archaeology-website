b19cd6d3ec4560698eac20325145f49402770d10
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-12 12:27:26 -0400

    updating database to use .env file even in development mode

--- app/api/database.js
+++ app/api/database.js
@@ -1,17 +1,11 @@
 import postgres from 'postgres';
 
-let Database;
-if(process.env.NODE_ENV === "development")
-    Database = await import('../assets/sessions/database-env.json').then(response => {
-        return postgres(response);
-    });
-else
-    Database = postgres({
-        database: process.env.DBNAME,
-        username: process.env.DBUSER,
-        password: process.env.DBPASS,
-        host: process.env.DBHOST,
-        port: process.env.DBPORT,
-        ssl: process.env.DBMODE
-    }); 
+const Database = postgres({
+    database: process.env.DBNAME,
+    username: process.env.DBUSER,
+    password: process.env.DBPASS,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    ssl: process.env.DBMODE
+}); 
 export default Database;
