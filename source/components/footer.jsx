894eeb38b82f2779d05af154f59f83be5a35a5ad
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-12 15:50:34 -0400

    provided footer styles

--- source/components/footer.jsx
+++ source/components/footer.jsx
@@ -8,6 +8,133 @@ import {Heading, Subheading} from './common/typography';
 import Button from './common/button';
 import Modal from './common/modal';
 
+const useStyles = tss.create(({theme}) => ({
+    footer: {
+        width: "100%",
+        backgroundColor: theme.surface,
+        color: theme.body
+    },
+    content: {
+        maxWidth: "1500px",
+        margin: "auto",
+        display: "flex",
+        gap: "40px",
+        padding: "20px"
+    },
+    locations: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        width: "33%",
+        "@media (max-width: 800px)": { // On mobile the location are put into a popup
+            display: "none"
+        },
+
+        "& .location": {
+            display: "flex",
+            flexDirection: "column"
+        },
+        "& .divider": {
+            height: "1pt",
+            width: "66%",
+            alignSelf: "center",
+            backgroundColor: theme.body,
+            opacity: 0.5
+        }
+    },
+    links: {
+        width: "34%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        "@media (max-width: 800px)": { // Links are the only objects shown in the footer on mobile
+            width: "100%",
+            flexDirection: "row",
+        },
+
+        "& a": { // Resetting the links
+            textDecoration: "none",
+            color: theme.body
+        }
+    },
+    navigation: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        "@media (max-width: 800px)": { // When on mobile the navigation is 50% of the footer
+            width: "50%",
+        },
+
+        "& .active": {
+            fontWeight: "bold"
+        }
+    },
+    socials: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        "@media (max-width: 800px)": { // When on mobile the socials are 50% of the footer
+            width: "50%"
+        },
+
+        "& .icons": {
+            display: "flex",
+            gap: "10px",
+
+            "& .urban-icons": {
+                fontSize: "32px"
+            }
+        },
+
+        // This contact button only appears on mobile under the socials
+        "& .contact": {
+            display: "none",
+            "@media (max-width: 800px)": {
+                display: "block",
+                alignSelf: "center",
+                marginTop: "30px"
+            }
+        }
+    },
+    credits: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+        textAlign: "center",
+        backgroundColor: theme.primary,
+        color: theme.onPrimary,
+
+        "& a": {
+            textDecoration: "none",
+            color: theme.onSecondary  
+        },
+
+        "& .accredation": {
+            fontSize: "0.75rem",
+            padding: "10px",
+        }
+    },
+    modal: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        overflow: "auto",
+
+        "& .location": {
+            display: "flex",
+            flexDirection: "column"
+        },
+        "& .divider": {
+            height: "1pt",
+            width: "66%",
+            alignSelf: "center",
+            backgroundColor: theme.body,
+            opacity: 0.5
+        }
+    }
+}));
+
 /**
  * This is the footer of the website. It will hold the company location/contact information,
  * as well as, the information about the copyright and accredation. When the website is on mobile
@@ -16,18 +143,21 @@ import Modal from './common/modal';
 export default function Footer() {
     const [open, setOpen] = useState(false); // This keeps track of if the popup is open or not
     const pathname = usePathname();
+
+    const theme = useTheme();
+    const {classes} = useStyles({theme});
     return (
-        <section className='footer'>
-            <div className='info'>
-                <div className='links'>
-                    <div className='navigation'>
+        <section className={classes.footer}>
+            <div className={classes.content}>
+                <div className={classes.links}>
+                    <div className={classes.navigation}>
                         <Heading>Navigation</Heading>
                         <Link className={pathname === '/'? "active" : ""} href="/">Home</Link>
                         <Link className={pathname === '/catalog'? "active" : ""} href="/catalog">Catalog</Link>
                         <Link className={pathname === '/salvage'? "active" : ""} href="/salvage">Salvage</Link>
                         <Link className={pathname === '/gallery'? "active" : ""} href="/gallery">Gallery</Link>
                     </div>
-                    <div className='socials'>
+                    <div className={classes.socials}>
                         <Heading>Socials</Heading>
                         <div className='icons'>
                             <Link className='urban-icons' href="https://www.facebook.com/urbanarchaeologyltd">facebook_logo</Link>
@@ -35,10 +165,10 @@ export default function Footer() {
                             <Link className='urban-icons' href="https://pinterest.com/urbanarchltd/">pinterest_logo</Link>
                             <Link className='urban-icons' href="https://www.linkedin.com/company/urban-archaeology/">linkedin_logo</Link>
                         </div>
-                        <Button className='contact' role="primary" style="filled" onPress={() => setOpen(true)}>Contact Us</Button>
+                        <Button className='contact' role="primary" style="filled" onClick={() => setOpen(true)}>Contact Us</Button>
                     </div>
                 </div>
-                <div className='locations'>
+                <div className={classes.locations}>
                     <Heading>Locations</Heading>
                     <div className='location'>
                         <Subheading>New York (Showroom)</Subheading>
@@ -58,7 +188,7 @@ export default function Footer() {
                         />
                     </div>
                 </div>
-                <div className='locations'>
+                <div className={classes.locations}>
                     <div className='location'>
                         <Subheading>Long Island City (Workshop)</Subheading>
                         <span>43-34 32nd Place, 2R, Long Island City, NY 11101</span>
@@ -83,16 +213,16 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className='credits'>
-                <span className='copyright'>Copyright © 2012-2024 Urban Archaeology Ltd. All rights reserved.</span>
-                <Link className='terms' href="/terms">Terms and conditions apply.</Link>
+            <div className={classes.credits}>
+                <span>Copyright © 2012-2024 Urban Archaeology Ltd. All rights reserved.</span>
+                <Link href="/terms">Terms and conditions apply.</Link>
                 <span className='accredation'>Designed and built by <Link href="https://www.kalvingarcia.com">Kalvin Garcia</Link></span>
             </div>
 
             <Modal open={open} setOpen={setOpen}>
-                <div className='locations-popup'>
+                <div className={classes.modal}>
                     <Heading>Locations</Heading>
-                    <div className='location'>
+                    <div className={classes.locations}>
                         <Subheading>New York (Showroom)</Subheading>
                         <span>158 Franklin Street, New York, NY 10013</span>
                         <span>(212) 371-4646</span>
