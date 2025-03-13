0ddeba97d5a013b64360d40a10ffdb88a27c3db6
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-13 14:57:22 -0400

    styled spotlight component

--- source/components/spotlight.jsx
+++ source/components/spotlight.jsx
@@ -1,19 +1,66 @@
 "use client"
 import {useState, useEffect, cloneElement, useCallback, Children} from "react";
+import {tss} from 'tss-react';
+import {useTheme} from "./common/theme";
 
-function Stage({lead = false, mini = false, onClick, children}) {
+const stageStyles = tss.create(({theme, mini, lead}) => ({
+    stage: {
+        flex: mini? "0 0 auto" : "none",
+        width: mini? "150px" : "100%",
+        height: mini? "150px" : "auto",
+        filter: mini && !lead? "grayscale(100%)" : "none",
+        border: mini && lead? `1pt solid ${theme.onPrimary}` : "none",
+        aspectRatio: 1,
+
+        "& img": {
+            height: "100%",
+            minWidth: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+        },
+
+        "@media (max-width: 600px)": {
+            width: mini? "100px" : "100%",
+            height: mini? "100px" : "auto"
+        }
+    }
+}));
+function Stage({mini = false, lead = false, onClick, children}) {
     const handleClick = useCallback(() => {
         if(mini)
             onClick();
     }, [mini]);
 
+    const theme = useTheme();
+    const {classes} = stageStyles({theme, mini, lead});
     return (
-        <figure className={["stage", lead? "lead" : "", mini? "mini" : ""].join(" ")} onClick={handleClick}>
+        <figure className={classes.stage} onClick={handleClick}>
             {children}
         </figure>
     );
 }
 
+const spotlightStyles = tss.create(({theme}) => ({
+    spotlight: {
+        maxWidth: "500px",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+
+        "@media (max-width: 1100px)": {
+            maxWidth: "none"
+        }
+    },
+    backstage: {
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        height: "fit-content",
+        width: "100%",
+        overflowX: "auto"
+    }
+}));
 export default function Spotlight({children}) {
     const [activeStage, setActiveStage] = useState(0);
     const [stages, setStages] = useState(() => {
@@ -29,10 +76,12 @@ export default function Spotlight({children}) {
         )));
     }, [activeStage]);
 
+    const theme = useTheme();
+    const {classes} = spotlightStyles({theme});
     return (
-        <div className="spotlight">
+        <div className={classes.spotlight}>
             {stages[activeStage]}
-            <div className="backstage">
+            <div className={classes.backstage}>
                 {stages.map(stage => (
                     cloneElement(stage, {mini: true})
                 ))}
