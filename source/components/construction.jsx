5f40c1e94ed723200d5845de53e4db439eb86b3a
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-17 11:28:05 -0400

    styled construction component

--- source/components/construction.jsx
+++ source/components/construction.jsx
@@ -1,17 +1,35 @@
+"use client"
+import {tss} from 'tss-react';
+import {useTheme} from './common/theme';
 import {Title} from './common/typography';
 import Button from './common/button';
 
+const useStyles = tss.create(({theme}) => ({
+    construction: {
+        padding: "40px",
+        minHeight: "400px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px"
+    },
+    divider: {
+        height: "1pt",
+        width: "75%",
+        backgroundColor: theme.body + "7F"
+    }
+}));
 
 // https://forms.gle/c6KUMZ9wDvTvRyDKA
 export default function Construction({title, children}) {
+    const theme = useTheme();
+    const {classes} = useStyles({theme});
     return (
-        <div className="construction">
+        <div className={classes.construction}>
             <Title>{title}</Title>
-            <div className="divider" />
+            <div className={classes.divider} />
             <p>{children}</p>
-            <a href="https://forms.gle/c6KUMZ9wDvTvRyDKA" target='_blank'>
-                <Button>See Design Concepts</Button>
-            </a>
+            <Button onClick={() => setTimeout(() => window.location.href = "https://projects.kalvingarcia.com/?open=urbarch-website", 100)}>See Design Concepts</Button>
         </div>
     );
 }
